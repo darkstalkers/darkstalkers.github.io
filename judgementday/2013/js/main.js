0e1775a8f3590b60bde7e2ea465f959e19911913
Toru Yoshikawa <pikotea@gmail.com>
2013-11-06 20:39:01 +0900

    mobile and social

--- judgementday/2013/js/main.js
+++ judgementday/2013/js/main.js
@@ -1,1 +1,6 @@
-
+// anchor links
+$(document).on('click', 'a.list-group-item', function(e){
+  e.preventDefault();
+  var to = $($(this).attr('href')).position().top - 25;
+  $('body').animate({scrollTop: to}, 300);
+});
