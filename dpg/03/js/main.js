025024b1bbb1c9fa13b3c97e2fdcf2c456f6c6c7
Toru Yoshikawa <pikotea@gmail.com>
2014-03-05 20:28:14 +0900

    update scroll

--- dpg/03/js/main.js
+++ dpg/03/js/main.js
@@ -2,10 +2,10 @@
   var body = document.body;
   if ( window.innerWidth > 600 ) {
     window.addEventListener('scroll', function(e){
-      body.style.backgroundPosition = '50% ' + parseInt(window.scrollY / 1.5) + 'px';
+      body.style.backgroundPosition = '50% ' + (- parseInt(window.scrollY / 1.5)) + 'px';
     }, false);
   } else {
-    body.style.backgroundAttachment = 'fixed';
+    // body.style.backgroundAttachment = 'fixed';
   }
 
   $('.fancybox').fancybox({
