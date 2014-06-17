194b170ddc24c5d8fa6f44fe61844e3070303aa8
Toru Yoshikawa <pikotea@gmail.com>
2014-06-17 19:25:30 +0900

    add effect

--- judgementday/2014/js/main.js
+++ judgementday/2014/js/main.js
@@ -1,7 +1,31 @@
 (function(){
+  var width = $(window).width();
+  var left = Math.floor(width / 2);
+  var top  = Math.floor($(window).height() / 2);
+
   var main = $('.main');
   main[0].style.cssText = 'display: none';
   setTimeout(function(){
-    $('.main').fadeIn(2000);
+    ripple(left, top);
+    $('.main').fadeIn(2500);
   }, 500);
+
+
+
+  $(document.body).bind("click",function(e) {
+    ripple(e.pageX, e.pageY);
+  });
+  function ripple(x, y){
+    new Ripple({
+        x: x,
+        y: y,
+        size: width,
+        count: 1,
+        width: 1,
+        color: "rgba(255,0,0,0.5)",
+        zIndex: 30,
+        shadow: '0 0 6px red',
+        duration: 2000
+    });
+  }
 })();
