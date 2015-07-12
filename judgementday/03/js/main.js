14c53644f6417d4d627ca34f37e7fdb55ac6a8af
Toru Yoshikawa <pikotea@gmail.com>
2015-07-13 02:44:20 +0900

    dpg3 release

---
+++ judgementday/03/js/main.js
@@ -0,0 +1,86 @@
+(function(){
+
+  // anchor links
+  var con = $('.container');
+  $(document).on('click', '.gnav a', function(e){
+    e.preventDefault();
+    var to = $($(this).attr('href')).position().top + con.scrollTop() - 5;
+    con.animate({scrollTop: to}, 300);
+  });
+
+  // smooth scroll
+  var bg = document.querySelector('.container');
+  if ( window.innerWidth > 600 ) {
+    bg.addEventListener('scroll', function(e){
+      bg.style.backgroundPosition = '0 ' + ((- parseInt(bg.scrollTop / 5.5))-200) + 'px';
+    }, false);
+  } else {
+    bg.style.backgroundSize = 'contain';
+  }
+
+  /* gnav */
+  $('#btn_menu').on('click', function () {
+    $('.gnav').toggleClass('menu_open');
+  });
+  $('body').on('click', function(e){
+    if ( window.innerWidth > 600 ) {
+      if (!e.originalEvent.srcElement.classList.contains('close-ignore')) {
+        $('.gnav').removeClass('menu_open');
+      }
+    } else {
+      if (e.originalEvent.srcElement.id !== 'btn_menu') {
+        $('.gnav').removeClass('menu_open');
+      }
+    }
+  });
+
+  // Entry list
+  $(window).load(function (e) {
+
+    // $(".fancybox, .fancybox").fancybox();
+    
+    // var isDisp = false,
+    //     dispBtn = $('#disp');
+    // dispBtn.click(function(e){
+    //   e.preventDefault();
+    //   if ( isDisp ) {
+    //     dispBtn.text('エントリーリストを表示する');
+    //     isDisp = false;
+    //   } else {
+    //     dispBtn.text('エントリーリストを非表示にする');
+    //     isDisp = true;
+    //   }
+    //   $('.tab-content').animate({ height: 'toggle' });
+      
+    });
+
+
+    // order asc
+    // savior.trio = savior.trio.reverse();
+    // savior.pair = savior.pair.reverse();
+    // savior.solo = savior.solo.reverse();
+
+    // $('#savior .loading').hide();
+
+    // trio
+    // $('#savior-entry-list-item').tmpl(savior.trio).appendTo('#savior-entry-list > ul');
+
+    // pair
+    // if ( savior.pair.length !== 0 ) {
+    //   $('#savior-entry-list-item').tmpl(savior.pair).appendTo('#savior-entry-pair > ul');
+    // } else {
+    //   $('#savior-entry-pair').parent().hide();
+    // }
+
+    // solo
+    // $('#savior-entry-list-item').tmpl(savior.solo).appendTo('#savior-entry-solo > ul');
+
+    // free
+    // if ( savior.free.length !== 0 ) {
+    //   $('#savior-entry-single-tmpl').tmpl(savior.free).appendTo('#savior-entry-single > ul');
+    // } else {
+    //   $('#savior-entry-single').parent().hide();
+    // }
+  // });
+
+})();
