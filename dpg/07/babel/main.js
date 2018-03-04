9b49c9af5d2d6a02e5a07f10678a82522b0cb9b7
pikotea <pikotea@gmail.com>
2018-03-05 00:37:46 +0900

    DPG7のティザーを公開

---
+++ dpg/07/babel/main.js
@@ -0,0 +1,121 @@
+(function(){
+
+  // anchor links
+  var body = $('body');
+  $(document).on('click', '.gnav a', function(e){
+    e.preventDefault();
+    var to = $($(this).attr('href')).position().top - 5;
+    if ( window.innerWidth < 768 ) {
+      to -= 45;
+    }
+    body.animate({scrollTop: to}, 300);
+  });
+
+  // smooth scroll
+  // var bg = document.querySelector('bpdy');
+  // if ( window.innerWidth > 600 ) {
+  //   if ( navigator.userAgent.match(/Chrome\/(46|47|48)/) ) {
+  //     bg.style.backgroundAttachment = 'initial';
+  //     bg.style.backgroundSize = 'initial';
+  //     bg.style.position = 'static';
+  //   } else {
+  //     bg.addEventListener('scroll', function(e){
+  //       bg.style.backgroundPosition = '0 ' + ((- parseInt(bg.scrollTop / 5.5))-200) + 'px';
+  //     }, false);
+  //   }
+  // } else {
+  //   bg.style.backgroundSize = 'contain';
+  // }
+
+  /* gnav */
+  $('#btn_menu').on('click', function () {
+    $('.gnav').toggleClass('menu_open');
+  });
+  $('body').on('click', function(e){
+    if ( window.innerWidth > 600 ) {
+      if (!e.originalEvent.srcElement.classList.contains('close-ignore')) {
+        $('.gnav').removeClass('menu_open');
+      }
+    } else {
+      if (e.originalEvent.srcElement.id !== 'btn_menu') {
+        $('.gnav').removeClass('menu_open');
+      }
+    }
+  });
+
+  // Entry list
+  $(window).load(function (e) {
+
+    $(".fancybox, .fancybox").fancybox({
+      padding: 10
+    });
+    
+    // var isDisp = false,
+    //     dispBtn = $('#disp');
+    // dispBtn.click(function(e){
+    //   e.preventDefault();
+    //   if ( isDisp ) {
+    //     dispBtn.text('エントリーリストを表示する');
+    //     isDisp = false;
+    //   } else {
+    //     dispBtn.text('エントリーリストを非表示にする');
+    //     isDisp = true;
+    //   }
+    //   $('.tab-content').animate({ height: 'toggle' });
+
+    });
+
+
+    // order asc
+    // savior.trio = savior.trio.reverse();
+    // savior.pair = savior.pair.reverse();
+    // savior.solo = savior.solo.reverse();
+
+    // $('#savior .loading').hide();
+
+    // trio
+    // $('#savior-entry-list-item').tmpl(savior.trio).appendTo('#savior-entry-list > ul');
+
+    // pair
+    // if ( savior.pair.length !== 0 ) {
+    //   $('#savior-entry-list-item').tmpl(savior.pair).appendTo('#savior-entry-pair > ul');
+    // } else {
+    //   $('#savior-entry-pair').parent().hide();
+    // }
+
+    // solo
+    // $('#savior-entry-list-item').tmpl(savior.solo).appendTo('#savior-entry-solo > ul');
+
+    // free
+    // if ( savior.free.length !== 0 ) {
+    //   $('#savior-entry-single-tmpl').tmpl(savior.free).appendTo('#savior-entry-single > ul');
+    // } else {
+    //   $('#savior-entry-single').parent().hide();
+    // }
+  // });
+
+  const windowOpen = (url, width, height) => {
+    let screenWidth = screen.width;
+    let screenHeight = screen.height;
+    let left = Math.round((screenWidth / 2) - (width / 2));
+    let top = 0;
+    if (screenHeight > height) {
+      top = Math.round((screenHeight / 2) - (height / 2));
+    }
+    let options = 'scrollbars=yes,resizable=yes,toolbar=no,location=yes,' +
+                `width=${width},height=${height},left=${left},top=${top}`;
+    window.open(url, null, options);
+  };
+
+  $('.share__button--facebook').on('click', function(e) {
+    e.preventDefault();
+    windowOpen(this.href, 550, 600);
+    return false;
+  });
+  $('.share__button--twitter').on('click', function(e) {
+    e.preventDefault();
+    windowOpen(this.href, 550, 350);
+    return false;
+  });
+
+})();
